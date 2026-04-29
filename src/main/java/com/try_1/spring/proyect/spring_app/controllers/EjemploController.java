433c6684b386aced116f10abbb002d5438182074
ayleenPascuales <ayleenpascuales@gmail.com>
2026-04-28 20:29:10 -0500

    feat: prueba de diseño visual

---
+++ src/main/java/com/try_1/spring/proyect/spring_app/controllers/EjemploController.java
@@ -0,0 +1,16 @@
+package com.try_1.spring.proyect.spring_app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class EjemploController {
+    
+    @GetMapping("/inicio")
+    
+    public String info(){
+        
+        return "inicio";
+    }
+}
